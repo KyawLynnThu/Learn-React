c462ee80c236a37dd2c6e316f44d01d10e0ceab4
Kyaw Linn Thu <67432058+KyawLynnThu@users.noreply.github.com>
2023-08-21 14:13:53 +0630

    styling

--- src/components/HeyNow.js
+++ src/components/HeyNow.js
@@ -1,7 +1,13 @@
+import "./HeyNow.css"
+
 function HeyNow() {
     return (
         <div>
-            <h1>Index is 1</h1>
+            <h1 style={{
+                backgroundColor: "blue",
+                color: "white"
+            }}>Index is 1</h1>
+            <h2>Index is 2</h2>
         </div>
     )
 }
