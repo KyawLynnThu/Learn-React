3044c497725d2121a04aa1f5d2c30c4c0c600bed
Kyaw Linn Thu <67432058+KyawLynnThu@users.noreply.github.com>
2023-08-22 09:52:14 +0630

    useState

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import User from "./components/users/User";
 function App() {
   let users = [
@@ -23,15 +23,25 @@ function App() {
       cell: "098080808080",
       name: "Kg Kg"
     }
-  ]
-  return (    
+  ];
+
+  let [count, setCount] = useState(users.length); // [ variable, method to change variable's value ]= defaultValue
+
+  return (
     <div className="container my-5">
+      <div>
+        <h1>{count}</h1>
+      </div>
       {/* First Way
       <User image={users[0].image} phone={users[0].phone} cell={users[0].cell} name={users[0].name} />
       <User image={users[1].image} phone={users[1].phone} cell={users[1].cell} name={users[1].name} /> */}
 
       {/* Second Way
       <User data={users[0]} /> */}
+
+      <button className="btn btn-info btn-sm my-5" onClick={() => {
+        setCount(++count);
+      }}>Change</button>
 
       {/* Third Way */}
       {
