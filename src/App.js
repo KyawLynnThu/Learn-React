c62ddc2ce3e4b0b737af3663e39b65fee0e8352f
Kyaw Linn Thu <67432058+KyawLynnThu@users.noreply.github.com>
2023-08-26 16:01:37 +0630

    Handling input onchange data

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
 import React, { useEffect, useState } from "react";
 import User from "./components/users/User";
+import AddUser from "./components/users/AddUser";
 
 function App() {
   let [ users, setUsers ] = useState([]);
+  let [ showForm, setShowForm ] = useState(false);
 
   useEffect(() => {
     fetch("https://randomuser.me/api/?results=10")
@@ -27,11 +29,25 @@ function App() {
     let remainUsers = users.filter(usr => usr.uuid !== uuid);
     setUsers(remainUsers)
   }
+
+  const addUserHandler = (user) => {
+    console.log(user);
+    let newUsers = [ user, ...users]
+    setUsers(newUsers)
+    setShowForm(!showForm)
+  }
+
+  const showFormHandler = () => {
+    setShowForm(!showForm);
+  }
    
   return (
     <div className="container my-5">
       <div>
         <h1 className="text-center my-5 text-info">Our Employee</h1>
+        <button className="btn btn-primary btn-sm my-2" onClick={showFormHandler}>Add User</button>
+        { showForm && <AddUser addUser={addUserHandler} /> }
+        <br></br>
         {
           users.map(usr => <User key={usr.uuid} user={usr} remove={removeUserHandler} />)
         }
