d21ea96fffd1f8fab7b7be202f867339e8aa07a8
Kyaw Linn Thu <67432058+KyawLynnThu@users.noreply.github.com>
2023-08-29 16:11:43 +0630

    using styled components

--- src/components/CompoA.js
+++ src/components/CompoA.js
@@ -1,12 +1,26 @@
-import "../styles/compo.css"
+import React from "react";
+import { styled } from "styled-components";
 
+const Container = styled.div`
+    width: 350px;
+    heightL 150px;
+    background-color: grey;
+
+    h1 {
+        color: red;
+    }
+
+    h2 {
+        color: white;
+    }
+`
 function CompoA() {
     return(
-        <div className="container">
+        <Container>
             <h1>CompoA Title</h1>
             <h2>CompoA</h2>
-      </div >
+        </Container >
     )
-}
+} 
 
 export default CompoA; 
--- src/components/CompoB.js
+++ src/components/CompoB.js
@@ -1,8 +1,21 @@
+import React from "react";
+import { styled } from "styled-components";
+
+const Button = styled.button`
+    border: none;
+    border-radius: 10px;
+    background-color: hotpink;
+    color: white;
+    padding: 5px 10px;
+
+    &:hover {
+        background-color: blue;
+    }
+`
 function CompoB() {
     return(
-        <div className="container">
-            <h1>CompoB Title</h1>
-            <h2>CompoB</h2>
+        <div>
+            <Button>Click Me</Button>
       </div >
     )
 }
